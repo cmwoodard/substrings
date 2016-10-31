a8360aa70d4d0a225af194fa13478994c8b53fe6
Mitch Woodard <woodardcommamitch@gmail.com>
2016-10-31 09:52:44 -0500

    update tosubstrings

--- Substrings.rb
+++ Substrings.rb
@@ -6,13 +6,11 @@ def substrings(phrase, dictionary)
 	
 	dictionary.each{|dic_element|
 		phrase.each{|phrase_element|
-		#puts "#{dic_element} vs #{phrase_element.downcase} = #{(phrase_element.downcase).include? dic_element}"
 			word_hash["#{dic_element}"]+=1 if (phrase_element.downcase).include? dic_element
 		}
 	}
-	word_hash.sort
+
 	word_hash.each{|one, two| puts "#{one}: #{two}" if two>0}
-	word_hash.sort
 end
 
 words = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
